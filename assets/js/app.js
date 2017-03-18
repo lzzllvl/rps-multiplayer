e75ae2c680ffe9ecfb52dce7dcc56d3a2f37bb61
Greg Lee <gml2136@gmail.com>
2017-03-17 21:16:53 -0700

    firebase is not helping me out with this one

---
+++ assets/js/app.js
@@ -0,0 +1,95 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCXXB_tEegxmS3qIqo8JuVL14vmClvFLRM",
+  authDomain: "gl-rps-multiplayer.firebaseapp.com",
+  databaseURL: "https://gl-rps-multiplayer.firebaseio.com",
+  storageBucket: "gl-rps-multiplayer.appspot.com",
+  messagingSenderId: "495578356922"
+};
+firebase.initializeApp(config);
+const database = firebase.database();
+
+var rpsGame = {
+  init: function(first, second) {
+    this.p1 = first ;
+    this.p2 = second;
+    this.turn = first;
+  },
+  makeMove: function(player, choice) {
+    if(player == this.p1) {
+      this.playerMove = choice;
+      this.turn = this.p2;
+    } else {
+      this.findResult(this.playerMove, choice);
+    }
+  },
+  findResult: function (user, opponent) {
+    if(user == opponent) {
+      return this.CONST.tie;
+    }
+    else if(user == "r") {
+      return opponent == "p" ? this.CONST.loss: this.CONST.win;
+    }
+    else if(user == "p") {
+      return opponent == "s" ? this.CONST.loss: this.CONST.win;
+    }
+    else if(user == "s") { // this `else if` technically could be just an `else`, but I feel it makes the code easier to understand
+      return opponent == "r" ? this.CONST.loss: this.CONST.win;
+    }
+  },
+  CONST: {
+    win: "Congrats! You Won!",
+    loss: "Too Bad, You Lost.",
+    tie: "It's a tie, meh."
+  }
+}
+
+
+function player(name) {
+  self = this;
+  this.init = function(name) {
+    self.wins = 0;
+    self.losses = 0;
+    self.name = name;
+  };
+  this.wonGame = function(){
+    self.wins++;
+  };
+  this.lostGame = function() {
+    self.losses++;
+  };
+  getPlayerNumber = function() {
+    database.ref("players").child("one").exists() ? self.player_number = "two": self.player_number = "one";
+  };
+}
+
+
+var gameController = {
+
+  storeUser: function(user) {
+    var player = "player" + this.player_number;
+    database.ref().set({"players" : { user.player_number : { "name": user.name,
+                                     "wins": user.wins,
+                                     "losses": user.losses}}});
+  },
+  acceptMove: function() {},
+  submitMove: function() {
+  },
+  opponentListener: function() {
+    database.ref("players").orderByValue().on("child_added", function(snapshot) {
+      console.log(snapshot);
+    })
+  },
+  removeUser: function() {
+
+  }
+}
+
+var chatController = {
+  submitMessage: function() {
+
+  },
+  updateScroll: function () {
+
+  }
+}
